25bdae5af5d4490bb1e339aba6bd638b1cfaca60
Eric Tuvesson <eric.tuvesson@gmail.com>
2018-05-08 00:04:54 +0200

    Update tests

--- test/Shapes/CubeTest.cs
+++ test/Shapes/CubeTest.cs
@@ -5,70 +5,32 @@ namespace CSG.Shapes
 
     public class CubeTest
     {
-        [Fact]
-        public void Intersect()
+        [Theory]
+        [InlineData(ShapeOperation.Intersect)]
+        [InlineData(ShapeOperation.Subtract)]
+        [InlineData(ShapeOperation.Union)]
+        public void ShapeOperations(ShapeOperation operation)
         {
             var shape1 = new Cube(new Vector3(0, 0, 0), new Vector3(0.50f, 0.50f, 0.50f));
             var shape2 = new Cube(new Vector3(1, 1, 0), new Vector3(0.50f, 0.50f, 0.50f));
-            var result = shape1.Intersect(shape2);
+            var result = shape1.Do(operation, shape2);
 
-            Assert.True(result.Vertices.Length > 0);
-            Assert.True(result.Indices.Length > 0);
+            Assert.True(result.Cache.Vertices.Length > 0);
+            Assert.True(result.Cache.Indices.Length > 0);
         }
 
-        [Fact]
-        public void Subtract()
-        {
-            var shape1 = new Cube(new Vector3(0, 0, 0), new Vector3(0.50f, 0.50f, 0.50f));
-            var shape2 = new Cube(new Vector3(1, 1, 0), new Vector3(0.50f, 0.50f, 0.50f));
-            var result = shape1.Subtract(shape2);
-
-            Assert.True(result.Vertices.Length > 0);
-            Assert.True(result.Indices.Length > 0);
-        }
-
-        [Fact]
-        public void Union()
-        {
-            var shape1 = new Cube(new Vector3(0, 0, 0), new Vector3(0.50f, 0.50f, 0.50f));
-            var shape2 = new Cube(new Vector3(1, 1, 0), new Vector3(0.50f, 0.50f, 0.50f));
-            var result = shape1.Union(shape2);
-
-            Assert.True(result.Vertices.Length > 0);
-            Assert.True(result.Indices.Length > 0);
-        }
-
-        [Fact]
-        public void Intersect_PolygonOverlapping()
+        [Theory]
+        [InlineData(ShapeOperation.Intersect)]
+        [InlineData(ShapeOperation.Subtract)]
+        [InlineData(ShapeOperation.Union)]
+        public void ShapeOperationsOverlapping(ShapeOperation operation)
         {
             var shape1 = new Cube(new Vector3(0, 0, 0), new Vector3(1.00f, 0.50f, 1.00f));
             var shape2 = new Cube(new Vector3(0, 0, 0), new Vector3(0.50f, 0.50f, 0.50f));
-            var result = shape1.Intersect(shape2);
+            var result = shape1.Do(operation, shape2);
 
-            Assert.True(result.Vertices.Length > 0);
-            Assert.True(result.Indices.Length > 0);
-        }
-
-        [Fact]
-        public void Subtract_PolygonOverlapping()
-        {
-            var shape1 = new Cube(new Vector3(0, 0, 0), new Vector3(1.00f, 0.50f, 1.00f));
-            var shape2 = new Cube(new Vector3(0, 0, 0), new Vector3(0.50f, 0.50f, 0.50f));
-            var result = shape1.Subtract(shape2);
-
-            Assert.True(result.Vertices.Length > 0);
-            Assert.True(result.Indices.Length > 0);
-        }
-
-        [Fact]
-        public void Union_PolygonOverlapping()
-        {
-            var shape1 = new Cube(new Vector3(0, 0, 0), new Vector3(1.00f, 0.50f, 1.00f));
-            var shape2 = new Cube(new Vector3(0, 0, 0), new Vector3(0.50f, 0.50f, 0.50f));
-            var result = shape1.Union(shape2);
-
-            Assert.True(result.Vertices.Length > 0);
-            Assert.True(result.Indices.Length > 0);
+            Assert.True(result.Cache.Vertices.Length > 0);
+            Assert.True(result.Cache.Indices.Length > 0);
         }
     }
 }
--- test/Shapes/SphereTest.cs
+++ test/Shapes/SphereTest.cs
@@ -5,37 +5,32 @@
 
     public class SphereTest
     {
-        [Fact]
-        public void Intersect()
+        [Theory]
+        [InlineData(ShapeOperation.Intersect)]
+        [InlineData(ShapeOperation.Subtract)]
+        [InlineData(ShapeOperation.Union)]
+        public void ShapeOperations(ShapeOperation operation)
         {
             var shape1 = new Sphere(new Vector3(0, 0, 0));
             var shape2 = new Sphere(new Vector3(1, 1, 0));
-            var result = shape1.Intersect(shape2);
+            var result = shape1.Do(operation, shape2);
 
-            Assert.True(result.Vertices.Length > 0);
-            Assert.True(result.Indices.Length > 0);
+            Assert.True(result.Cache.Vertices.Length > 0);
+            Assert.True(result.Cache.Indices.Length > 0);
         }
 
-        [Fact]
-        public void Subtract()
+        [Theory]
+        [InlineData(ShapeOperation.Intersect)]
+        [InlineData(ShapeOperation.Subtract)]
+        [InlineData(ShapeOperation.Union)]
+        public void ShapeOperationsOverlapping(ShapeOperation operation)
         {
             var shape1 = new Sphere(new Vector3(0, 0, 0));
-            var shape2 = new Sphere(new Vector3(1, 1, 0));
-            var result = shape1.Subtract(shape2);
+            var shape2 = new Sphere(new Vector3(0, 0, 0));
+            var result = shape1.Do(operation, shape2);
 
-            Assert.True(result.Vertices.Length > 0);
-            Assert.True(result.Indices.Length > 0);
-        }
-
-        [Fact]
-        public void Union()
-        {
-            var shape1 = new Sphere(new Vector3(0, 0, 0));
-            var shape2 = new Sphere(new Vector3(1, 1, 0));
-            var result = shape1.Union(shape2);
-
-            Assert.True(result.Vertices.Length > 0);
-            Assert.True(result.Indices.Length > 0);
+            Assert.True(result.Cache.Vertices.Length > 0);
+            Assert.True(result.Cache.Indices.Length > 0);
         }
     }
 }
