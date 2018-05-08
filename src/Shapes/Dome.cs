0f6f03bf0dd16d43d15c0af186108b43c9d09f4a
Eric Tuvesson <eric.tuvesson@gmail.com>
2018-05-08 21:37:31 +0200

    Add dummy classes of future shapes

---
+++ src/Shapes/Dome.cs
@@ -0,0 +1,12 @@
+﻿namespace CSG.Shapes
+{
+    using System.Numerics;
+
+    class Dome : Shape
+    {
+        protected override void OnBuild()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
