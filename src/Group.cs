d0fe66429730ed5699b90c82966c204286035b07
Eric Tuvesson <eric.tuvesson@gmail.com>
2018-10-11 23:58:08 +0200

    Update src/Group.cs

--- src/Group.cs
+++ src/Group.cs
@@ -1,7 +1,5 @@
 ﻿namespace CSG
 {
-    using CSG.Serialization;
-    using CSG.Shapes;
     using System;
     using System.Runtime.Serialization;
 
@@ -12,47 +10,29 @@
         public ShapeOperation Operation { get; set; }
 
         [DataMember]
-        public Shape Shape1 { get; set; }
+        public Shape Value1 { get; set; }
 
         [DataMember]
-        public Shape Shape2 { get; set; }
+        public Shape Value2 { get; set; }
 
-        public Group(ShapeOperation operation, Cube shape1, Cube shape2)
+        public Group(ShapeOperation operation, Shape value1, Shape value2)
         {
             this.Operation = operation;
-            this.Shape1 = shape1;
-            this.Shape2 = shape2;
+            this.Value1 = value1;
+            this.Value2 = value2;
         }
-
-        public Group(SerializationInfo info, StreamingContext context)
-        {
-            Enum.TryParse<ShapeOperation>(info.GetString("operation"), out var operation);
-            this.Operation = operation;
-
-            this.Shape1 = info.GetValue<Shape>("shape1");
-            this.Shape2 = info.GetValue<Shape>("shape2");
-        }
-
+        
         public GeneratedShape Do()
         {
-            if (Shape1 == null || Shape2 == null)
+            if (Value1 == null || Value2 == null)
                 return null;
 
-            return Shape1.Do(Operation, Shape2);
+            return Value1.Do(Operation, Value2);
         }
-
-        #region ISerializable
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            info.AddValue("operation", Operation.ToString());
-            info.AddValue("shape1", Shape1);
-            info.AddValue("shape2", Shape2);
-        }
-        #endregion
-
+        
         public bool Equals(Group other)
         {
-            return Shape1.Equals(other.Shape1) && Shape2.Equals(other.Shape2)
+            return Value1.Equals(other.Value1) && Value2.Equals(other.Value2)
                    && Operation == other.Operation;
         }
     }
