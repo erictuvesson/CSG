14e75d304b80d0a90dbc1b2215e2ef0b0ba5d8c0
Eric Tuvesson <eric.tuvesson@gmail.com>
2019-02-01 11:12:46 +0100

    Update Cone

--- samples/CSG.Viewer/App.cs
+++ samples/CSG.Viewer/App.cs
@@ -10,7 +10,7 @@
 
     class App : Application
     {
-        private CommandList _cl;
+        private CommandList commandList;
         private BasicMaterial basicMaterial;
         private Geometry geometry;
 
@@ -29,29 +29,29 @@
 
             geometry = new Geometry(DrawingContext, shape.Cache.Vertices, shape.Cache.Indices);
 
-            _cl = factory.CreateCommandList();
+            commandList = factory.CreateCommandList();
         }
 
         protected override void Draw(float deltaSeconds)
         {
             _ticks += deltaSeconds * 1000f;
-            _cl.Begin();
+            commandList.Begin();
 
             basicMaterial.Projection = Matrix4x4.CreatePerspectiveFieldOfView(1.0f, (float)Window.Width / Window.Height, 0.5f, 100f);
             basicMaterial.View = Matrix4x4.CreateLookAt(Vector3.UnitZ * 2.5f, Vector3.Zero, Vector3.UnitY);
             basicMaterial.World = Matrix4x4.CreateFromAxisAngle(Vector3.UnitY, (_ticks / 1000f)) * 
                                   Matrix4x4.CreateFromAxisAngle(Vector3.UnitX, (_ticks / 3000f));
 
-            _cl.SetFramebuffer(DrawingContext.MainSwapchain.Framebuffer);
-            _cl.ClearColorTarget(0, RgbaFloat.Black);
-            _cl.ClearDepthStencil(1f);
+            commandList.SetFramebuffer(DrawingContext.MainSwapchain.Framebuffer);
+            commandList.ClearColorTarget(0, RgbaFloat.Black);
+            commandList.ClearDepthStencil(1f);
 
-            basicMaterial.Apply(_cl);
-            geometry.Draw(_cl);
+            basicMaterial.Apply(commandList);
+            geometry.Draw(commandList);
 
-            _cl.End();
+            commandList.End();
 
-            DrawingContext.GraphicsDevice.SubmitCommands(_cl);
+            DrawingContext.GraphicsDevice.SubmitCommands(commandList);
             DrawingContext.GraphicsDevice.SwapBuffers(DrawingContext.MainSwapchain);
             DrawingContext.GraphicsDevice.WaitForIdle();
         }
--- src/CSG/Shapes/Cone.cs
+++ src/CSG/Shapes/Cone.cs
@@ -1,12 +1,55 @@
 ﻿namespace CSG.Shapes
 {
+    using System;
     using System.Numerics;
+    using System.Runtime.Serialization;
 
-    class Cone : Shape
+    [DataContract]
+
+    public class Cone : Shape
     {
+        [DataMember]
+        public int Tessellation { get; set; }
+
+        public Cone(int tessellation = 32)
+        {
+            this.Tessellation = tessellation;
+        }
+
         protected override void OnBuild(IShapeBuilder builder)
         {
-            throw new System.NotImplementedException();
+            var offset = new Vector3(0, -0.25f, 0);
+
+            builder.AddVertex(Vector3.UnitY + offset, Vector3.UnitY);
+            builder.AddVertex(Vector3.Zero + offset, -Vector3.UnitY);
+
+            for (int i = 0; i < Tessellation; ++i)
+            {
+                Vector3 normal = GetCircleVector(i, Tessellation);
+
+                builder.AddVertex(normal + offset, normal);
+                
+                builder.AddIndex(0);
+                builder.AddIndex(2 + i);
+                builder.AddIndex(2 + (i + 1) % Tessellation);
+
+                builder.AddIndex(1);
+                builder.AddIndex(2 + (i + 1) % Tessellation);
+                builder.AddIndex(2 + i);
+            }
+        }
+
+        // <summary>
+        /// Helper method computes a point on a circle.
+        /// </summary>
+        static Vector3 GetCircleVector(int i, int tessellation)
+        {
+            float angle = i * Algorithms.Helpers.TwoPi / tessellation;
+
+            float dx = (float)Math.Cos(angle);
+            float dz = (float)Math.Sin(angle);
+
+            return new Vector3(dx, 0, dz);
         }
     }
 }
