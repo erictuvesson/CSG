23205937cdbdcb1dafc2e9e9648e1e8d0b6a908a
Eric Tuvesson <eric.tuvesson@gmail.com>
2019-02-03 20:45:11 +0100

    Add prefab example as XML

--- src/CSG/Prefab.cs
+++ src/CSG/Prefab.cs
@@ -1,10 +1,19 @@
 namespace CSG
 {
-    public class Prefab
-    {
-        public Prefab()
-        {
+    using System.Runtime.Serialization;
 
+    public class Prefab : ISerializable
+    {
+        public string Name { get; private set; }
+
+        public Prefab(string name = "")
+        {
+            this.Name = name;
+        }
+
+        public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("name", Name);
         }
     }
 }
