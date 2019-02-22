5e19074c80d99499d8720bf1a3d1378deb16c8e5
Eric Tuvesson <eric.tuvesson@gmail.com>
2019-02-22 08:43:25 +0100

    Add Deserialize to Serializer

--- src/CSG/Serialization/Serializer.cs
+++ src/CSG/Serialization/Serializer.cs
@@ -2,6 +2,7 @@ namespace CSG.Serialization
 {
     using System;
     using System.IO;
+    using System.IO.Compression;
 
     public enum SerializeFormat
     {
@@ -25,16 +26,47 @@ namespace CSG.Serialization
         {
             if (binary)
             {
-                // TODO: Add gzip
-                
-                Stream stream = new FileStream(filepath, FileMode.Create);
-                SerializerStream.Serialize(value, ref stream);
-                stream.Close();
+                using (var fileStream = new FileStream(filepath, FileMode.Create))
+                {
+                    Stream stream = new GZipStream(fileStream, CompressionMode.Compress);
+                    SerializerStream.Serialize(value, ref stream);
+                    stream.Close();
+                }
             }
             else
             {
                 var content = SerializerStream.SerializeContent(value);
                 File.WriteAllText(filepath, content);
+            }
+        }
+
+        public T Deserialize<T>(string filepath, bool binary = false)
+        {
+            if (!File.Exists(filepath))
+            {
+                throw new FileNotFoundException($"File doesn't exists. (${filepath})");
+            }
+
+            if (binary)
+            {
+                var bytes = DeserializeBinary(filepath);
+                return SerializerStream.Deserialize<T>(bytes);
+            }
+            else
+            {
+                var content = File.ReadAllText(filepath);
+                return SerializerStream.DeserializeContent<T>(content);
+            }
+        }
+
+        private byte[] DeserializeBinary(string filepath)
+        {
+            using (var fileStream = new FileStream(filepath, FileMode.Open))
+            {
+                using (var stream = new GZipStream(fileStream, CompressionMode.Decompress))
+                {
+                    return stream.ReadToEnd();
+                }
             }
         }
 
