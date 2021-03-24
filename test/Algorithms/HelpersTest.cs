88b0d8fcea78aeb4f141e74c50f3295ba95bb1c8
Eric Tuvesson <eric.tuvesson@gmail.com>
2021-03-24 19:28:04 +0100

    feat: Add unit test for Helpers Clamp

---
+++ test/Algorithms/HelpersTest.cs
@@ -0,0 +1,20 @@
+﻿namespace CSG.Algorithms
+{
+    using Xunit;
+
+    public class HelpersTest
+    {
+        [Theory]
+        // Min
+        [InlineData(10, 0, int.MaxValue, 10)]
+        [InlineData(10, 100, int.MaxValue, 100)]
+        // Max
+        [InlineData(5, 0, 10, 5)]
+        [InlineData(100, 0, 10, 10)]
+        public void Clamp(int value, int min, int max, int expected)
+        {
+            var result = Helpers.Clamp(value, min, max);
+            Assert.Equal(expected, result);
+        }
+    }
+}
