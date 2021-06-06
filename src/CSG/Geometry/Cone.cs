79a60a345bea3d669287c49b57690951abc7a1ce
Eric Tuvesson <eric.tuvesson@gmail.com>
2021-06-06 12:49:52 +0200

    feat: Create Geometry classes (#20)
    This is the first step to move away from using triangles inside the shapes.

---
+++ src/CSG/Geometry/Cone.cs
@@ -0,0 +1,39 @@
+﻿namespace CSG.Geometry
+{
+    using System;
+    using System.Numerics;
+
+    public static class Cone
+    {
+        public static void CreateSolid(this IGeometryBuilder builder)
+            => CreateSolid(builder, 32);
+
+        public static void CreateSolid(this IGeometryBuilder builder, int tessellation)
+        {
+            Vector3 offset = new Vector3(0, -0.25f, 0);
+
+            builder.AddVertex(Vector3.UnitY + offset, Vector3.UnitY);
+            builder.AddVertex(Vector3.Zero + offset, -Vector3.UnitY);
+
+            for (int i = 0; i < tessellation; ++i)
+            {
+                float angle = i * Algorithms.Helpers.TwoPi / tessellation;
+
+                float dx = MathF.Cos(angle);
+                float dz = MathF.Sin(angle);
+
+                Vector3 normal = new Vector3(dx, 0, dz);
+
+                builder.AddVertex(normal + offset, normal);
+
+                builder.AddIndex(0);
+                builder.AddIndex(2 + i);
+                builder.AddIndex(2 + (i + 1) % tessellation);
+
+                builder.AddIndex(1);
+                builder.AddIndex(2 + (i + 1) % tessellation);
+                builder.AddIndex(2 + i);
+            }
+        }
+    }
+}
---
+++ src/CSG/Geometry/Torus.cs
@@ -0,0 +1,55 @@
+﻿namespace CSG.Geometry
+{
+    using System;
+    using System.Numerics;
+
+    public static class Torus
+    {
+        public static void CreateSolid(this IGeometryBuilder builder)
+            => CreateSolid(builder, 32);
+
+        public static void CreateSolid(this IGeometryBuilder builder, int tessellation)
+        {
+            // First we loop around the main ring of the torus.
+            for (int i = 0; i < tessellation; ++i)
+            {
+                float outerAngle = i * Algorithms.Helpers.TwoPi / tessellation;
+
+                // Create a transform matrix that will align geometry to
+                // slice perpendicularly though the current ring position.
+                var transform = Matrix4x4.CreateTranslation(1, 0, 0) *
+                                Matrix4x4.CreateRotationY(outerAngle);
+
+                // Now we loop along the other axis, around the side of the tube.
+                for (int j = 0; j < tessellation; j++)
+                {
+                    float innerAngle = j * Algorithms.Helpers.TwoPi / tessellation;
+
+                    float dx = MathF.Cos(innerAngle);
+                    float dy = MathF.Sin(innerAngle);
+
+                    // Create a vertex.
+                    Vector3 normal = new Vector3(dx, dy, 0);
+                    Vector3 position = normal / 2.0f;
+
+                    position = Vector3.Transform(position, transform);
+                    normal = Vector3.TransformNormal(normal, transform);
+
+                    builder.AddVertex(position, normal);
+
+                    // And create indices for two triangles.
+                    int nextI = (i + 1) % tessellation;
+                    int nextJ = (j + 1) % tessellation;
+
+                    builder.AddIndex(i * tessellation + j);
+                    builder.AddIndex(i * tessellation + nextJ);
+                    builder.AddIndex(nextI * tessellation + j);
+
+                    builder.AddIndex(i * tessellation + nextJ);
+                    builder.AddIndex(nextI * tessellation + nextJ);
+                    builder.AddIndex(nextI * tessellation + j);
+                }
+            }
+        }
+    }
+}
